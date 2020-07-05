7223fae52ed72d0c60cfe80dd33c1dc569086d18
proiacm <proiacm@gmail.com>
2020-07-05 11:44:45 -0600

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 
 const totalBatteries = batteryBatches.reduce(function (total, battery){
-        return battery + total}, 0)
+        return battery + total})
     
